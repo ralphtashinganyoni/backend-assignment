4c6fc5a3cef3a2de4ff5138963fd49dc9c302ef9
Ralph <ralph@chikwamapay.com>
2024-10-09 14:24:27 +0200

    Added RabbitMq configs to appsettings.json

--- src/OT.Assessment.App/Program.cs
+++ src/OT.Assessment.App/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OT.Assessment.App.Model.Repository;
 using OT.Assessment.App.RabbitMq;
 using OT.Assessment.App.Services;
+using OT.Assessment.Common.RabbitMq.Config;
 using OT.Assessment.Common.RabbitMq.Connection;
 using System.Reflection;
 
@@ -15,6 +18,12 @@ builder.Services.AddSwaggerGen(options =>
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
+
+builder.Services.Configure<RabbitMqConfigSettings>(
+    builder.Configuration.GetSection("RabbitMq"));
+
+builder.Services.AddSingleton(a => a.GetRequiredService<IOptions<RabbitMqConfigSettings>>().Value);
+
 builder.Services.AddSingleton<IRabbitMqConnection>( new RabbitMqConnection());
 builder.Services.AddScoped<IMessageProducer, MessageProducer>();
 builder.Services.AddTransient<IWagerRepository, WagerRepository>();
